445f409256b8408f2bf84f03c86c5ec622483b49
Bliss <blissmaake140@gmail.com>
2022-12-12 08:16:27 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
+
+  if (word == reverseString){
+
+    return true
+
+  } else {
+    return false
+  }
+
+
 }
+
 
 /* 
   Add your pseudocode here
@@ -8,6 +22,11 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  So the isPalindrome function needs to take a word and see if it's the same front and back
+   I've created a function that going to take the word and parse each 
+  letter in it ,then it will be put it in condition 
+  the condition will return true if it isPalindrome and  if not it 
+  will return false
 */
 
 // You can run `node index.js` to view these console logs
